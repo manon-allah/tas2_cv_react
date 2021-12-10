09e62156cbbc72d4b1303e4e65f2b232a2f75a15
manon-allah <menatallah260@gmail.com>
2021-12-10 12:51:02 +0200

    first

---
+++ src/Home.js
@@ -0,0 +1,61 @@
+import react from "react";
+import './Home.css'
+
+function Home() {
+    return(
+        <div>
+                 <div className="container">
+                    <div className="left">
+                    <div className="text">
+                    <table>
+                    <tr>
+                      
+                      <td><b> Student</b></td>
+                    </tr>
+                    <tr>
+                        <td> <b>El bagour Menofia Egypt</b></td>
+                    </tr>
+                    <tr>
+                        <td> <b>01065317353</b></td>
+                    </tr>
+                </table> 
+                <a href="https://www.facebook.com/menna.mohsen.98837" target="_blank">Fcebook</a>
+                <a href="https://www.instagram.com/menna.allah.2000/" target="_blank">Instgram</a>
+                <a href="https://twitter.com/menna_allah19?t=-4KU2lIM59g0mLWwSEiKzA&s=09" target="_blank">Twitter</a>
+                <a href="https://www.linkedin.com/in/mena-allah-205141203/" target="_blank">Linkedin</a>
+                <a href="https://github.com/manon-allah" target="_blank">Github</a>
+            
+                </div>
+            </div>
+            <div className="right">
+                <div className="top">
+                    <h3>Education : </h3>
+                    <p>Student at faculty of computer and Information science Menofia Universty</p>
+                    <p>I am trained in tafra company</p>
+                    </div>
+                <div className="bottom">
+                    <h3>Skills : </h3>
+                    <div className="skills">
+                    <li>C++</li>
+                        <li>javascript</li>
+                        <li>Java</li>
+                        <li>OPP</li>
+                        <li>SQL</li>
+                        <li>problem solving</li>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>CSS3</li>
+                        <li>PHP</li>
+                        <li>React</li>
+                    </div> 
+                    <p><b>Name : </b> Menna allah mohsen bedda</p>
+                    <p><b>Department : </b> CS</p>   
+                
+                </div>
+            </div>
+        </div>
+             
+        </div>
+    )
+}
+export default Home
